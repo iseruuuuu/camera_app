fb728cf41c675a94bd31101501384e5d062757d9
iseruuuuu <ryutaro.iseki23@gmail.com>
2022-08-10 09:54:57 +0900

    fix: remove code

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,26 @@ import 'package:flutter/material.dart';
 import 'home_screen.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final cameras = await availableCameras();
-  final firstCamera = cameras.first;
-  runApp(MyApp(camera: firstCamera));
+  // WidgetsFlutterBinding.ensureInitialized();
+  // final cameras = await availableCameras();
+  // final firstCamera = cameras.first;
+  // runApp(MyApp(camera: firstCamera));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({
     Key? key,
-    required this.camera,
+    // required this.camera,
   }) : super(key: key);
 
-  final CameraDescription camera;
+  // final CameraDescription camera;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(camera: camera),
+      // home: HomeScreen(camera: camera),
+      home: HomeScreen(),
     );
   }
 }
